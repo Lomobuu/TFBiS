42c8bcfe61bbb08b8f206e663f89e96827e7b432
Simen Fossen <simen.fossen@bouvet.no>
2026-08-24 14:53:45 +0200

    terraform fmt

--- terraform/backend/variables.tf
+++ terraform/backend/variables.tf
@@ -2,20 +2,20 @@
 variable "resource_group_name" {
   description = "The name of the resource group to create the resources in."
   type        = string
-  default = "rg-vsps-tfstate-mgmt-weu-001"
+  default     = "rg-vsps-tfstate-mgmt-weu-001"
 }
 
 variable "location" {
   description = "The name of the location to create the resources in."
   type        = string
-  default = "westeurope"
+  default     = "westeurope"
 }
 
 variable "account_name" {
   description = "The name of this Storage account."
   type        = string
   nullable    = false
-  default = "stvspstfstateweu001"
+  default     = "stvspstfstateweu001"
 }
 
 variable "container_name" {
