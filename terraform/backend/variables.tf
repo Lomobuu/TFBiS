6fbce7c78280957727c152c82a4194511b6340e7
Simen Fossen <simen.fossen@bouvet.no>
2026-08-24 14:48:49 +0200

    hardcode names & variables for backend

--- terraform/backend/variables.tf
+++ terraform/backend/variables.tf
@@ -1,17 +1,21 @@
+# TODO USE BICEP TO SETUP BACKEND BECAUSE THIS IS HARDCODED...
 variable "resource_group_name" {
   description = "The name of the resource group to create the resources in."
   type        = string
+  default = "rg-vsps-tfstate-mgmt-weu-001"
 }
 
 variable "location" {
   description = "The name of the location to create the resources in."
   type        = string
+  default = "westeurope"
 }
 
 variable "account_name" {
   description = "The name of this Storage account."
   type        = string
   nullable    = false
+  default = "stvspstfstateweu001"
 }
 
 variable "container_name" {
