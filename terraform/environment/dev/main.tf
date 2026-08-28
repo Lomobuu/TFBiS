a312e43bea6469e70f5c0e97dfb7ce2aabb9d58d
Simen Fossen <simen.fossen@bouvet.no>
2026-08-28 10:26:44 +0200

    new name for kv

--- terraform/environment/dev/main.tf
+++ terraform/environment/dev/main.tf
@@ -20,7 +20,7 @@ module "data" {
 module "keyvault" {
   source = "../../modules/shared"
 
-  name                = "kv-myapp-dev" # or your own naming convention
+  name                = "kv-fznmyapp-dev" # or your own naming convention
   resource_group_name = data.azurerm_resource_group.this.name
   location            = local.location
   sku_name            = "standard"
